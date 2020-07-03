9212f6168ba16ac21a33484aafb750d099ef6d89
veronar <vishakha108@gmail.com>
2020-07-03 17:16:18 +0200

    need to work on simulator - most others done

--- simulator/WeatherTower.java
+++ simulator/WeatherTower.java
@@ -6,10 +6,10 @@ import simulator.vehicles.Coordinates;
 
 public class WeatherTower extends Tower {
     public String getWeather(Coordinates coordinates) {
-
+        return (WeatherProvider.getProvider().getCurrentWeather(coordinates));
     };
 
     public void changeWeather() {
-
+        this.conditionsChanged();
     };
 }
