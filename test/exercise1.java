15bf9d4d2e59e2095bb17d285118bfeeaf2ed204
veronar <vishakha108@gmail.com>
2020-07-03 10:27:47 +0200

    learnng java

---
+++ test/exercise1.java
@@ -0,0 +1,9 @@
+import things.*;
+
+public class exercise1 {
+    public static void main(String[] args){
+        Person boy = new Person("David", "Water", 34);
+
+        System.out.println(boy);
+    }
+}
